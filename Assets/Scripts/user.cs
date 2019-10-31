07913a28e245edcad9771d60942f5d5a881d2b1a
pailf <fabian.p4il@gmail.com>
2019-10-31 14:14:54 +0100

    Create Branch

---
+++ Assets/Scripts/user.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class user : MonoBehaviour
+{
+    public bool usertype;
+    public bool gender;
+    public string username;
+    public int pincode;
+    public int avatarID;
+    public Input inputUsername;
+    public Input inputPincode;
+    public Input inputPincodeConfirm;
+
+    private string newUsername;
+    private int newPincode;
+    private int newPincodeConfirm;
+    private bool newUsertype;
+    private bool newGender;
+    private int newAvatarID;
+
+    //Mehr Variablen
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public bool SaveUser()
+    {  
+        
+    
+
+        if (pincode != newPincode && newPincode == newPincodeConfirm)
+        {
+            pincode = newPincode;
+        }
+        else
+        {
+            //Fehler: Der selbe Pincode darf nicht nochmal genommen werden oder die neuen Pincodes stimmen nicht überein
+        }
+        username = newUsername;
+        usertype = newUsertype;
+        gender = newGender;
+        avatarID = newAvatarID;
+
+        //schreibe auf lokales System
+
+        return false;
+    }
+}
