c988ae50376c99adbeb4c4252812a4d9099f08c1
pailf <fabian.p4il@gmail.com>
2019-11-06 20:17:39 +0100

    categories with text

--- Assets/Scripts/changeText.cs
+++ Assets/Scripts/changeText.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class changeText : MonoBehaviour
 {
-    public void ChangeText
+    public void ChangeText(getAvatar)
+    {
+
+    }
     {
 
     }
